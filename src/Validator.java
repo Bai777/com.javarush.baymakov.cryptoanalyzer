64463cd35974f64996d1ef2b97d0a09ea09aa8ff
Чат бот <bai_777@mail.ru>
2025-10-20 10:40:46 +0300

    Создал класс Validator, и два метода isValidKey, isFileExists пока без реализации.

---
+++ src/Validator.java
@@ -0,0 +1,12 @@
+public class Validator {
+    public boolean isValidKey(int key, char[] alphabet) {
+        // Проверка ключа
+        //TODO
+        return false;
+    }
+    public boolean isFileExists(String filePath) {
+        // Проверка существования файла
+        //TODO
+        return false;
+    }
+}

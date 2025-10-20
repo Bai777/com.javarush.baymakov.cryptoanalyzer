333e3f312dc730f7bf59e7fbaab36fdd28793c09
Чат бот <bai_777@mail.ru>
2025-10-20 10:52:15 +0300

    В класс Cipher добавил алфавит, добавил getALPHABET

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,8 +1,12 @@
 public class Cipher {
-    private char[] alphabet;
-    public Cipher(char[] alphabet) {
-        this.alphabet = alphabet;
+    private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
+            'и','к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
+            'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
+
+    public static char[] getALPHABET() {
+        return ALPHABET;
     }
+
     public String encrypt(String text, int shift) {
         // Логика шифрования
         //TODO

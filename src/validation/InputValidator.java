28abcda59445c06758c5f2e3fb9cbca88dc8835d
Чат бот <bai_777@mail.ru>
2025-10-21 16:16:15 +0300

    Реализовал классы InputValidator, ValidationException

--- src/validation/InputValidator.java
+++ src/validation/InputValidator.java
@@ -1,14 +1,35 @@
 package validation;
 
+import cipher.Alphabet;
+import io.ValidationException;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class InputValidator {
-    public boolean isValidKey(int key, char[] alphabet) {
-        // Проверка ключа
-        //TODO
-        return false;
+
+    public void validateFileExists(String filePath) {
+        Path path = Paths.get(filePath);
+        if (!Files.exists(path)){
+            throw new ValidationException("Файл не существует: " + filePath);
+        }
+        if (!Files.isReadable(path)){
+            throw new ValidationException("Файл не читается: " + filePath);
+        }
     }
-    public boolean isFileExists(String filePath) {
-        // Проверка существования файла
-        //TODO
-        return false;
+
+    public void validateOutputPath(String filePath) {
+        try {
+            Paths.get(filePath);
+        } catch (Exception e) {
+            throw new ValidationException("Неверный путь вывода: " + filePath);
+        }
+    }
+
+    public void validateKey(int key, Alphabet alphabet) {
+        if (key < 0){
+            throw new ValidationException("Ключ не может быть отрицательным значением!");
+        }
     }
 }
